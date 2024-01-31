7fe33703c349266c2f968174701bd4337d555021
Mark Griffiths <markg1704@hotmail.com>
2024-01-31 21:06:03 +0000

    Initial Project Commit

---
+++ src/main/java/uk/co/markg1704/volumecalc/VolumeCalcApplication.java
@@ -0,0 +1,13 @@
+package uk.co.markg1704.volumecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolumeCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolumeCalcApplication.class, args);
+	}
+
+}
